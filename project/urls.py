fde17a7631f6c3edfe665e297dfabae9d6a3c832
Munawar Ali <138569548+MunawarAliArbisoft@users.noreply.github.com>
2023-08-09 15:14:28 +0500

    fix: changes token route name
    Change the name of api/token route to api/login to be more meaningful for user.

--- project/urls.py
+++ project/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('', include('project.api_urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/candidate/', include('candidate.urls')),
     path('api/assessment/', include('assessment.urls')),
